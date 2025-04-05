d22c486825e316c6af353f7c8995220b4dbda0e1
git_ross <ross_overbeek@gmail.com>
2025-04-05 16:24:49 -0500

    Revisions to 'README.md', '.vscode/settings.json', 'Scripts/course_env.sh', and the Download instructions.

--- Scripts/course_env.sh
+++ Scripts/course_env.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 shopt -s expand_aliases
 
-if [ -f ~/.bashrc ]; then
-  source ~/.bashrc
-fi
+# for f in ~/.bash /etc/bashrc /etc/bash.bashrc; do
+#  if [ -f "$f" ]; then
+#    source "$f"
+#  fi
+# done
 
 export COURSE_HOME="${COURSE_HOME:-$PWD}"
 cd "$COURSE_HOME"
 
 cdcourse() {
   echo "✅ COURSE_HOME is: $COURSE_HOME"
-  echo "🔁 Use 'cdcourse' anytime to return to this root."
+  echo "🔁 Use the command 'cdcourse' anytime to return to this root."
+  echo ""
   builtin cd "$COURSE_HOME"
   unset -f cdcourse  # only print once
   alias cdcourse='cd "$COURSE_HOME"'
 }
 alias cdcourse=cdcourse
+
+cdcourse
