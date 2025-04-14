47afa3dfc3d98905081c1540535c4f483ee6a15a
gdpusch <gdpusch@xnet.com>
2025-04-14 12:12:50 -0500

    revisions to 'Scripts/course_env.sh'.

--- Scripts/course_env.sh
+++ Scripts/course_env.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 shopt -s expand_aliases
 
-# for f in ~/.bash /etc/bashrc /etc/bash.bashrc; do
-#  if [ -f "$f" ]; then
-#    source "$f"
-#  fi
-# done
+for f in /etc/bashrc /etc/bash.bashrc ${HOME}/.bashrc; do
+  if [ -f "$f" ]; then
+    source "$f"
+  fi
+done
 
 export COURSE_HOME="${COURSE_HOME:-$PWD}"
 cd "$COURSE_HOME"
 
-cdcourse() {
+export PATH="$COURSE_HOME/Scripts:$COURSE_HOME/Code:$PATH"
+
+function cdcourse() {
   echo "✅ COURSE_HOME is: $COURSE_HOME"
   echo "🔁 Use the command 'cdcourse' anytime to return to this root."
   echo ""
